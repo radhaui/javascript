0220535ddca67a859bc045bc1106068b2783bf1a
radhaui <radhaui1612@gmail.com>
2024-12-13 15:31:46 +0530

    Update rotate-array-k-times.js

--- rotate-array-k-times.js
+++ rotate-array-k-times.js
@@ -13,3 +13,21 @@ const rotateArray = () => {
   return [...rotate, ...x]
 }
 console.log(rotateArray());
+
+
+///second approch
+var x=[1,2,3,4,5,6,7,];
+//[7,1,2,3,4,5,6] => [7,6,1,2,3,4,5] => [5,6,7,1,2,3,4]
+var k=14;
+const rotate = () => {
+	let size = x.length;
+  if( k > size) {
+  	k = k % size;
+  }
+
+  for (let i=0;i<k;i++){
+  	x.unshift(x.pop());
+  }
+  return x;
+}
+console.log(rotate());
