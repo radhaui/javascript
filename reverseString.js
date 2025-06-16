da1a7619201cbdeeb102ec260c5c4beb87d9b94e
radhaui <radhaui1612@gmail.com>
2025-06-16 14:31:04 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -14,3 +14,16 @@ while(left<right){
 
 str = str.join("");
   console.log(str);
+
+//2nd approch
+
+const  str="radha";
+ let reversStr= "";
+const strArray = str.split("");
+console.log(strArray);
+console.log(strArray[strArray.length-1]);
+for(var i=strArray.length-1;i>=0;i--){
+  reversStr += strArray[i];
+}
+console.log(reversStr);
+
