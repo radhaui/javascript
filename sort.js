595443b3d9378c309455ab5daa53d4d6b14aee19
radhaui <radhaui1612@gmail.com>
2025-09-19 12:13:55 +0530

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,12 @@
+const arr = [5,1,4,9,3,7,2];
+
+for(let i=0;i<arr.length;i++){
+  for(let j=0;j<arr.length-i-1;j++){
+    if(arr[j]>arr[j+1]){
+      const temp = arr[j];
+      arr[j]=arr[j+1];
+      arr[j+1]=temp;
+    }
+  }
+}
+console.log(arr);
